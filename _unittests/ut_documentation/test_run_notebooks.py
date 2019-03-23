e261a605e1d3126e340dae62274d30f477deb4d4
xavier dupré <xavier.dupre@gmail.com>
2019-03-23 23:20:09 +0100

    refactoring of unit test

--- _unittests/ut_documentation/test_run_notebooks.py
+++ _unittests/ut_documentation/test_run_notebooks.py
@@ -2,29 +2,12 @@
 """
 @brief      test log(time=36s)
 """
-
-import sys
 import os
 import unittest
 from pyquickhelper.loghelper import fLOG
 from pyquickhelper.pycode import ExtTestCase
 from pyquickhelper.ipythonhelper import test_notebook_execution_coverage
-
-
-try:
-    import src
-except ImportError:
-    path = os.path.normpath(
-        os.path.abspath(
-            os.path.join(
-                os.path.split(__file__)[0],
-                "..",
-                "..")))
-    if path not in sys.path:
-        sys.path.append(path)
-    import src
-
-import src.wrapclib
+import wrapclib
 
 
 class TestRunNotebooksPython(ExtTestCase):
@@ -37,6 +20,7 @@ class TestRunNotebooksPython(ExtTestCase):
 
         import jyquickhelper as jyq
         self.assertNotEmpty(jyq)
+        self.assertNotEmpty(wrapclib)
 
         folder = os.path.join(os.path.dirname(__file__),
                               "..", "..", "_doc", "notebooks")
--- _unittests/ut_re2/test_matches.py
+++ _unittests/ut_re2/test_matches.py
@@ -1,28 +1,9 @@
 """
 @brief      test log(time=3s)
 """
-
-import sys
-import os
 import unittest
 from pyquickhelper.pycode import ExtTestCase
-
-
-try:
-    import src
-except ImportError:
-    path = os.path.normpath(
-        os.path.abspath(
-            os.path.join(
-                os.path.split(__file__)[0],
-                "..",
-                "..")))
-    if path not in sys.path:
-        sys.path.append(path)
-    import src
-
-
-from src.wrapclib import re2
+from wrapclib import re2
 
 
 class TestMatch(ExtTestCase):
