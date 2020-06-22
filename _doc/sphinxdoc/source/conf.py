6a6b87f01e19e8103c4a750559c850dceb62bc3f
xavier dupré <xavier.dupre@gmail.com>
2020-06-22 10:03:45 +0200

    change theme

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-import cakephp_theme
+import alabaster
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
@@ -10,11 +10,10 @@ local_template = os.path.join(os.path.abspath(
 
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 set_sphinx_variables(__file__, "wrapclib", "Xavier Dupré", 2020,
-                     "cakephp_theme", cakephp_theme.get_html_theme_path(),
+                     "alabaster", alabaster.get_path(),
                      locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/wrapclib/issues/%s', 'issue')),
-                     title="wrapclib", book=True,
-                     add_extensions=['cakephp_theme'])
+                     title="wrapclib", book=True)
 
 blog_root = "http://www.xavierdupre.fr/app/wrapclib/helpsphinx/"
 
@@ -23,40 +22,8 @@ html_context = {
 }
 
 html_logo = "phdoc_static/project_ico.png"
-
 html_sidebars = {}
-
 language = "en"
-custom_preamble = """\n
-\\newcommand{\\vecteur}[2]{\\pa{#1,\\dots,#2}}
-\\newcommand{\\N}[0]{\\mathbb{N}}
-\\newcommand{\\indicatrice}[1]{\\mathbf{1\\!\\!1}_{\\acc{#1}}}
-\\usepackage[all]{xy}
-\\newcommand{\\infegal}[0]{\\leqslant}
-\\newcommand{\\supegal}[0]{\\geqslant}
-\\newcommand{\\ensemble}[2]{\\acc{#1,\\dots,#2}}
-\\newcommand{\\fleche}[1]{\\overrightarrow{ #1 }}
-\\newcommand{\\intervalle}[2]{\\left\\{#1,\\cdots,#2\\right\\}}
-\\newcommand{\\loinormale}[2]{{\\cal N}\\pa{#1,#2}}
-\\newcommand{\\independant}[0]{\\;\\makebox[3ex]{\\makebox[0ex]{\\rule[-0.2ex]{3ex}{.1ex}}\\!\\!\\!\\!\\makebox[.5ex][l]{\\rule[-.2ex]{.1ex}{2ex}}\\makebox[.5ex][l]{\\rule[-.2ex]{.1ex}{2ex}}} \\,\\,}
-\\newcommand{\\esp}{\\mathbb{E}}
-\\newcommand{\\var}{\\mathbb{V}}
-\\newcommand{\\pr}[1]{\\mathbb{P}\\pa{#1}}
-\\newcommand{\\loi}[0]{{\\cal L}}
-\\newcommand{\\vecteurno}[2]{#1,\\dots,#2}
-\\newcommand{\\norm}[1]{\\left\\Vert#1\\right\\Vert}
-\\newcommand{\\dans}[0]{\\rightarrow}
-\\newcommand{\\partialfrac}[2]{\\frac{\\partial #1}{\\partial #2}}
-\\newcommand{\\partialdfrac}[2]{\\dfrac{\\partial #1}{\\partial #2}}
-\\newcommand{\\loimultinomiale}[1]{{\\cal M}\\pa{#1}}
-\\newcommand{\\trace}[1]{tr\\pa{#1}}
-\\newcommand{\\abs}[1]{\\left|#1\\right|}
-"""
-# \\usepackage{eepic}
-
-imgmath_latex_preamble += custom_preamble
-latex_elements['preamble'] += custom_preamble
-mathdef_link_only = True
 
 epkg_dictionary.update({
     'C': 'https://en.wikipedia.org/wiki/C_(programming_language)',
