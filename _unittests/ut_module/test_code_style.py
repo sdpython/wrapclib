140047b5f3e4445ea7edb1cf993694357934163d
xavier dupré <xavier.dupre@gmail.com>
2022-06-13 13:26:25 +0200

    lint

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -14,7 +14,7 @@ class TestCodeStyle(ExtTestCase):
         thi = os.path.abspath(os.path.dirname(__file__))
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "wrapclib"))
         check_pep8(src_, fLOG=fLOG,
-                   pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705',
+                   pylint_ignore=('C0103', 'C1801', 'R1705',
                                   'W0108', 'W0613', 'W0212'),
                    skip=["gitsrc/re2", "gitsrc\\re",
                          "gitsrc/benchlog", "gitsrc\\benchlog"])
@@ -23,7 +23,7 @@ class TestCodeStyle(ExtTestCase):
         thi = os.path.abspath(os.path.dirname(__file__))
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
-                   pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705',
+                   pylint_ignore=('C0103', 'C1801', 'R1705',
                                   'W0108', 'W0613', 'C0111', 'W0703',
                                   'C0116'),
                    skip=[])
